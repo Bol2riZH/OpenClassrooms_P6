590e4a06af880af4ec8dddd118736f4f63aa1f49
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-13 15:12:49 +0200

    jwt to create token

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,20 @@ exports.signup = (req, res) => {
 exports.login = (req, res) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      if (!user)
+      if (!user) {
         res.status(401).json({ message: 'Incorrect email or password ' });
-      else res.status(200).json({ message: 'Connexion authorized' });
+      }
+      bcrypt.compare(req.body.password, user.password).then(valid => {
+        if (!valid) {
+          res.status(401).json({ message: 'Incorrect email or password ' });
+        }
+        res.status(200).json({
+          userId: user._id,
+          token: jwt.sign({ userId: user._id }, 'secretRamdomKey', {
+            expiresIn: '24h',
+          }),
+        });
+      });
     })
     .catch(error => res.status(400).json({ error }));
 };

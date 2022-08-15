950513835dc2caba3bae431433520d46e0a80bf9
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-15 09:29:03 +0200

    update and delete sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,6 @@
 const Sauce = require('../models/Sauce');
 
-exports.addNewSauces = (req, res) => {
+exports.createSauce = (req, res) => {
   const sauce = new Sauce({
     ...req.body,
   });
@@ -11,14 +11,29 @@ exports.addNewSauces = (req, res) => {
     .catch(error => res.status(400).json({ message: 'error:' + error }));
 };
 
-exports.showListOfSauces = (req, res) => {
+exports.getAllSauces = (req, res) => {
   Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 };
 
-exports.showOneSauce = (req, res) => {
+exports.getOneSauce = (req, res) => {
   Sauce.findById(req.params.id)
     .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.updateSauce = (req, res) => {
+  Sauce.findByIdAndUpdate(req.params.id, {
+    ...req.body,
+    _id: req.params.id,
+  })
+    .then(() => res.status(200).json({ message: 'Sauce update !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauce = (req, res) => {
+  Sauce.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).json({ message: 'Sauce deleted !' }))
     .catch(error => res.status(400).json({ error }));
 };
--- routes/sauce.js
+++ routes/sauce.js
@@ -5,8 +5,10 @@ const auth = require('../middleware/auth');
 
 const saucesCtrl = require('../controllers/sauce');
 
-router.post('/', auth, saucesCtrl.addNewSauces);
-router.get('/', auth, saucesCtrl.showListOfSauces);
-router.get('/:id', auth, saucesCtrl.showOneSauce);
+router.post('/', auth, saucesCtrl.createSauce);
+router.post('/:id', auth, saucesCtrl.updateSauce);
+router.post('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 
 module.exports = router;
